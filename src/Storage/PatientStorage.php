dc4bf9cfada21f5192e8ffdd4e1b44a8f646533f
Luis Diego Cordero H <lcordero@wearegap.com>
2017-03-14 20:22:26 +0000

    PILOT-4689: Add functionalidad to get patient from Intergys by user id

--- src/Storage/PatientStorage.php
+++ src/Storage/PatientStorage.php
@@ -63,10 +63,53 @@ class PatientStorage extends AbstractStorage
      * @param   Integer     $practiceId     Id of the practice we want to search
      * @return  Object                      List of patients that matches with the search
      */
-    public function findPatient( $patientId, $practiceId=null )
+    public function getPatient( $patientId, $practiceId=null )
     {
-        $patients = $this->searchPatient($patientId, $practiceId);
-        return ( !empty($patients) && count($patients) > 0 ) ? $patients[0] : null;
+        // Get the pactice Id in case we haven't received one
+        if( empty($practiceId) )
+        {
+            $practiceId = $this->getDefaultPractice();
+        }
+
+        // We need to be logged into the practice we want to search
+        $this->logonToPractice( $practiceId, $this->userId, $this->licenseId, $this->sessionId, $this->userLogon, $this->applicationName );
+
+        // Get Url to the search patient endpoint
+        $uri = $this->getUrl( 'patient-summary' );
+
+        // Create a Request
+        $queryData = [
+            'Credential' => [
+                "LicenseID" => $this->licenseId,
+                "SessionID" => $this->sessionId,
+                "UserLogon" => $this->userLogon,
+                "ApplicationName" => $this->applicationName,
+                "UserMachineName" => "",
+            ],
+            "PatientID" => $patientId
+        ];
+
+        // Send the request to the server and wait for the response
+        $response = $this->callAPI( 'POST', $uri, $queryData );
+        return $this->getUser( $response );
+    }
+
+    /**
+     * Extract the practice id from the Practice object
+     * This function will be used by a Mapping function
+     *
+     * @param   Object     $response    Response returned by the server when searching for users
+     * @return  Array                   List of users
+     */
+    private function getUser( $response )
+    {
+        // Checks parameters
+        if( empty($response) )
+        {
+            throw new MissingParameterException( __FUNCTION__ );
+        }
+
+        return $response->PersonDetails;
     }
 
     /**
